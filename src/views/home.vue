71e510bafb410d51d9460f3f49b322cdca19d10e
mac <mac@macdeMacBook-Pro-2.local>
2019-08-02 23:24:50 +0800

    模版工程初始化

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    这是一个VUE2.0工程搭建的VW移动端适配方案工程模版
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    width:750px;
+    height: 1300px;
+    background-color: yellowgreen;
+  }
+</style>
+
